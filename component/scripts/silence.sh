5a421b8e7ca5d7c76a5c52c9eed7cbe57fce2178
Sebastian Widmer <sebastian.widmer@vshn.net>
2022-09-23 14:37:11 +0200

    fix bash loop

--- component/scripts/silence.sh
+++ component/scripts/silence.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 curl_opts=( https://alertmanager-main.openshift-monitoring.svc.cluster.local:9095/api/v2/silences --cacert /etc/ssl/certs/serving-certs/service-ca.crt --header 'Content-Type: application/json' --header "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" --resolve "alertmanager-main.openshift-monitoring.svc.cluster.local:9095:$(getent hosts alertmanager-operated.openshift-monitoring.svc.cluster.local | awk '{print $1}' | head -n 1)" --silent )
 
-for silence in $(printf %s "${SILENCES_JSON}" | jq -cr '.[]'); do
+while IFS= read -r silence; do
   comment=$(printf %s "${silence}" | jq -r '.comment')
 
   body=$(printf %s "$silence" | \
@@ -20,4 +20,4 @@ for silence in $(printf %s "${SILENCES_JSON}" | jq -cr '.[]'); do
   fi
 
   curl "${curl_opts[@]}" -XPOST -d "${body}"
-done
+done <<<"$(printf %s "${SILENCES_JSON}" | jq -cr '.[]')"
