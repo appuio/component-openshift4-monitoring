75a46023053e4cee87277c4cc636f7f6077d85e2
Sebastian Widmer <sebastian.widmer@vshn.net>
2022-09-23 11:48:55 +0200

    Fail on error and use shellcheck recommendations

--- component/scripts/silence.sh
+++ component/scripts/silence.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-curl_opts=( https://alertmanager-main.openshift-monitoring.svc.cluster.local:9095/api/v2/silences --cacert /etc/ssl/certs/serving-certs/service-ca.crt --header 'Content-Type: application/json' --header "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" --resolve alertmanager-main.openshift-monitoring.svc.cluster.local:9095:$(getent hosts alertmanager-operated.openshift-monitoring.svc.cluster.local | awk '{print $1}' | head -n 1) --silent )
+curl_opts=( https://alertmanager-main.openshift-monitoring.svc.cluster.local:9095/api/v2/silences --cacert /etc/ssl/certs/serving-certs/service-ca.crt --header 'Content-Type: application/json' --header "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" --resolve "alertmanager-main.openshift-monitoring.svc.cluster.local:9095:$(getent hosts alertmanager-operated.openshift-monitoring.svc.cluster.local | awk '{print $1}' | head -n 1)" --silent )
 
 comment='Silence non syn alerts'
 
-read -d '' body << EOF
+read -rd '' body << EOF
 {
   "matchers": [
     {
@@ -20,7 +21,7 @@ read -d '' body << EOF
   ],
   "startsAt": "$(date -u +'%Y-%m-%dT%H:%M:%S')",
   "endsAt": "$(date -u +'%Y-%m-%dT%H:%M:%S' --date '+1 year')",
-  "createdBy": "Kubernetes object `cronjob/silence` in the monitoring namespace",
+  "createdBy": "Kubernetes object \`cronjob/silence\` in the monitoring namespace",
   "comment": "${comment}"
 }
 EOF
